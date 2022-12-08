f5f27c2e15f25602182ade60ad59e515670bb194
Jacky_tut <netfinnov@mail.ru>
2022-12-08 22:05:19 +0300

    task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Random rnd = new Random();
+int number = rnd.Next(100, 1000);
+
+Console.WriteLine($"Число равно {number}");
+
+int firstDigit = number / 10;
+int secondDigit = firstDigit % 10;
+
+Console.WriteLine($"Вторая цифра равна {secondDigit}");
+
+
